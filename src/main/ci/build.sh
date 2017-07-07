05f7fee7c2059117840a05bbce71ac6533acc831
Tim Otto <tim@timotto.io>
2017-07-07 10:46:11 +0200

    script cleanup

--- src/main/ci/build.sh
+++ src/main/ci/build.sh
@@ -1,24 +1,18 @@
 #!/bin/sh -ex
 
-froms=$(cat \
-  ${java_src}/${java_tag}/${dockerfile_path} \
-  ${node_src}/${node_tag}/${dockerfile_path} \
-  | grep ^FROM \
-  | uniq \
-  | wc -l)
-
-if [ $froms = 1 ]; then
-  echo "FROM: $froms" 1>&2
-else
-  echo "Different base images, cannot combine:" 1>&2
+combine() {
   cat \
     ${java_src}/${java_tag}/${dockerfile_path} \
     ${node_src}/${node_tag}/${dockerfile_path} \
-    | grep ^FROM \
-    1>&2
+}
+
+froms() { combine | grep ^FROM ; }
+
+froms
+
+if [ $(froms | uniq | wc -l) -ne 1 ]; then
+  echo "Different base images, cannot combine:" 1>&2
+  exit 1
 fi
 
-cat \
-  ${java_src}/${java_tag}/${dockerfile_path} \
-  ${node_src}/${node_tag}/${dockerfile_path} \
-  > ${target}/Dockerfile
+combine > ${target}/Dockerfile
--- src/main/ci/commit.sh
+++ src/main/ci/commit.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-git clone java-node-dockerfile-source updated-java-node-dockerfile-source
-tar cC java-node-dockerfile-build . | tar xC updated-java-node-dockerfile-source
-git -C updated-java-node-dockerfile-source \
-add .
-git -C updated-java-node-dockerfile-source \
--c user.name="${git_user}" \
--c user.email="${git_email}" \
-commit \
-  -q \
-  -m "${commit_message}"
+build=updated-java-node-dockerfile-source
+
+git clone java-node-dockerfile-source ${build}
+
+tar cC java-node-dockerfile-build . | tar xC ${build}
+
+git -C ${build} add .
+
+git -C ${build} \
+  -c user.name="${git_user}" \
+  -c user.email="${git_email}" \
+  commit \
+    -q \
+    -m "${commit_message}"
