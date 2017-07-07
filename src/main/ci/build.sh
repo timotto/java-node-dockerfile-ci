9baa30121b64c49b4676bd51dc20a620e3a0f6a5
Tim Otto <tim@timotto.io>
2017-07-07 10:40:28 +0200

    bugfix

--- src/main/ci/build.sh
+++ src/main/ci/build.sh
@@ -7,15 +7,15 @@ froms=$(cat \
   | uniq \
   | wc -l)
 
-if [ ! $froms = 1 ]; then
+if [ $froms = 1 ]; then
+  echo "FROM: $froms" 1>&2
+else
   echo "Different base images, cannot combine:" 1>&2
   cat \
     ${java_src}/${java_tag}/${dockerfile_path} \
     ${node_src}/${node_tag}/${dockerfile_path} \
     | grep ^FROM \
-    > 1>&2
-else
-  echo "FROM: $froms" 1>&2
+    1>&2
 fi
 
 cat \
